0bfa04a69d827d5e31176c3081ef8cfba17af06b
Matt Hayes <matthayes@yahoo-inc.com>
2014-05-21 10:38:50 -0700

    A button-y surface in the middle that changes colors when you click.

--- app/src/main.js
+++ app/src/main.js
@@ -4,13 +4,14 @@ define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
 
+    var Random = require('math/Random');
     var AppView = require('views/AppView');
 
     // create the main context
     var mainContext = Engine.createContext();
 
     // your app here
-    var app = new AppView()
+    var app = new AppView();
 
     mainContext.add(app);
 });
--- app/src/views/AppView.js
+++ app/src/views/AppView.js
@@ -2,8 +2,9 @@
 define(function(require, exports, module) {
     var View = require('famous/core/View');
     var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
-    var Modifier = require('famous/core/Modifier');
+    // var Transform = require('famous/core/Transform');
+    // var Modifier = require('famous/core/Modifier');
+    var StateModifier = require('famous/modifiers/StateModifier');
 
     var Random = require('math/Random');
 
@@ -16,23 +17,36 @@ define(function(require, exports, module) {
     function AppView() {
         View.apply(this, arguments);
 
-        var logo = new Surface({
-            size: [200, 200],
-            classes: ['backfaceVisibility'],
+        var gain = new StateModifier({
+            origin: [0.5, 0.5]
+        });
+
+        var main = new Surface({
             properties: {
                 backgroundColor: Random.hex()
             }
         });
 
-        var initialTime = Date.now();
-        var centerSpinModifier = new Modifier({
+        var node = new Surface({
+            size: [100, 100],
             origin: [0.5, 0.5],
-            transform : function() {
-                return Transform.rotateY(.002 * (Date.now() - initialTime));
+            align: [0.5, 0.5],
+            properties: {
+                backgroundColor: Random.hex(),
+                boxShadow: '0 0 30px 1px rgba(0,0,0,0.35)',
+                cursor: 'pointer'
             }
         });
 
-        this.add(centerSpinModifier).add(logo);
+        node.on('click', function() {
+            node.setProperties({
+                backgroundColor: Random.hex()
+            });
+        });
+
+        this.gain = this.add(gain);
+        this.gain.add(main);
+        this.gain.add(node);
     }
 
     AppView.prototype = Object.create(View.prototype);

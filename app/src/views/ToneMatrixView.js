bcf2b07edf4a8946f7d7f3462c3d46f2e36725c6
Matt Hayes <matthayes@yahoo-inc.com>
2014-05-21 13:13:20 -0700

    Refactor into separate views and grid layout.

--- app/src/views/ToneMatrixView.js
+++ app/src/views/ToneMatrixView.js
@@ -2,8 +2,13 @@
 define(function(require, exports, module) {
     var View = require('famous/core/View');
     var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
+    // var Transform = require('famous/core/Transform');
+    // var StateModifier = require('famous/modifiers/StateModifier');
+    var GridLayout = require('famous/views/GridLayout');
+
+    var Random = require('math/Random');
+
+    var ToneView = require('views/ToneView');
 
     /*
      * @name ToneMatrixView
@@ -13,6 +18,20 @@ define(function(require, exports, module) {
 
     function ToneMatrixView() {
         View.apply(this, arguments);
+
+        var d = 16;
+        var grid = new GridLayout({
+            dimensions: [d, d]
+        });
+
+        var tones = [];
+        grid.sequenceFrom(tones);
+
+        for (var i = 0, len = d * d; i < len; ++i) {
+            tones.push(new ToneView());
+        }
+
+        this.add(grid);
     }
 
     ToneMatrixView.prototype = Object.create(View.prototype);
